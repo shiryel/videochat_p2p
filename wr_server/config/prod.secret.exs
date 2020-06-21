a4a1024cb88d550aba87eefefba071449f652b63
Vinicius <35617139+shiryel@users.noreply.github.com>
2020-06-20 21:39:11 -0300

    Update prod.secret.exs

--- wr_server/config/prod.secret.exs
+++ wr_server/config/prod.secret.exs
@@ -23,7 +23,7 @@ config :wr_server, WrServerWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :wr_server, WrServerWeb.Endpoint, server: true
-#
+config :wr_server, WrServerWeb.Endpoint, server: true
+
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
